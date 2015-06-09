7f8b968a634a6a048b6e42f37c984c61447a057e
lifelonglearner127 <life.long.learner127@outlook.com>
2015-06-09 12:36:37 +0300

    [cleanup] rails

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,9 @@ require "action_view/railtie"
 Bundler.require(*Rails.groups)
 
 module GymcloudApi
+
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -31,5 +33,7 @@ module GymcloudApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
   end
+
 end

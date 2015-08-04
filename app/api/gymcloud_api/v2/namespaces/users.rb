85b2e8c13b5aa68db9f3d5c30a4e423ab52e46e7
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-04 18:37:09 +0300

    [add] endpoints: user collections

--- app/api/gymcloud_api/v2/namespaces/users.rb
+++ app/api/gymcloud_api/v2/namespaces/users.rb
@@ -13,6 +13,18 @@ class Users < Base
     get do
     end
 
+    %w{
+      exercises workout_templates program_templates
+      personal_workouts personal_programs
+      personal_properties workout_events
+    }.each do |collection|
+      namespace collection.to_sym do
+        desc "Fetch #{collection.titleize}"
+        get do
+        end
+      end
+    end
+
   end
 
 end

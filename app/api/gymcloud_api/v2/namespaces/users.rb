59a2ba255ac4b2aa4e660bca57f66148ac0474c5
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-04 19:19:11 +0300

    [fix] users endpoint collection namespace

--- app/api/gymcloud_api/v2/namespaces/users.rb
+++ app/api/gymcloud_api/v2/namespaces/users.rb
@@ -13,16 +13,24 @@ class Users < Base
     get do
     end
 
-    %w{
-      exercises workout_templates program_templates
-      personal_workouts personal_programs
-      personal_properties workout_events
-    }.each do |collection|
-      namespace collection.to_sym do
-        desc "Fetch #{collection.titleize}"
-        get do
+    desc 'Invite User'
+    post :invite
+
+    namespace :collections do
+
+      %w{
+        pros clients
+        exercises workout_templates program_templates
+        personal_workouts personal_programs
+        personal_properties workout_events
+      }.each do |collection|
+        namespace collection.to_sym do
+          desc "Fetch #{collection.titleize}"
+          get do
+          end
         end
       end
+
     end
 
   end

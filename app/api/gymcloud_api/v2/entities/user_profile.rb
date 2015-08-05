57492a32efc71bc3569a3f19a5d4690a138b26c5
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-05 22:08:38 +0300

    [add] UserProfile api implementation

---
+++ app/api/gymcloud_api/v2/entities/user_profile.rb
@@ -0,0 +1,20 @@
+module GymcloudAPI::V2
+module Entities
+
+class UserProfile < Grape::Entity
+  expose :id
+  expose :gender
+  expose :height
+  expose :weight
+  expose :bodyfat
+  expose :first_name
+  expose :last_name
+  expose :location
+  expose :zip
+  expose :employer
+  expose :birthday
+  expose :user_id
+end
+
+end
+end

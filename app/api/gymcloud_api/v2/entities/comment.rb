5a3e10fdebfde6e547d983fa195c656ee8ae8f89
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-06 19:33:10 +0300

    [add] Comments api implementation

---
+++ app/api/gymcloud_api/v2/entities/comment.rb
@@ -0,0 +1,15 @@
+module GymcloudAPI::V2
+module Entities
+
+class Comment < Grape::Entity
+  expose :id
+  expose :title
+  expose :comment
+  expose :commentable_id
+  expose :commentable_type
+  expose :role
+  expose :user, using: Entities::User
+end
+
+end
+end
--- app/api/gymcloud_api/v2/namespaces/comments.rb
+++ app/api/gymcloud_api/v2/namespaces/comments.rb
@@ -4,18 +4,43 @@ module Namespaces
 class Comments < Base
 
   desc 'Create Comment'
-  post
+  params do
+    requires :title, type: String, default: ''
+    requires :comment, type: String
+    requires :commentable_id, type: Integer
+    requires :commentable_type, type: String
+    requires :role, type: String, default: 'comments'
+  end
+  post do
+    attributes = filtered_params.merge(user: current_user).to_h
+    present ::Comment.create!(attributes), with: Entities::Comment
+  end
 
+  params do
+    requires :id, type: Integer, desc: 'Comment ID'
+  end
   route_param :id do
 
     desc 'Read Comment'
-    get
+    get do
+      present ::Comment.find(filtered_params[:id]), with: Entities::Comment
+    end
 
     desc 'Update Comment'
-    patch
+    params do
+      optional :title, type: String
+      optional :comment, type: String
+    end
+    patch do
+      comment = ::Comment.find params[:id]
+      comment.update_attributes! filtered_params.to_h
+      present comment, with: Entities::Comment
+    end
 
     desc 'Delete Comment'
-    delete
+    delete do
+      present ::Comment.destroy(params[:id]), with: Entities::Comment
+    end
 
   end
 

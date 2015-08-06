3a18ccbc006a29a346f3726193ebdf11112bc022
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-06 19:33:10 +0300

    [add] Comments api implementation

--- app/api/gymcloud_api/v2/entities/comment.rb
+++ app/api/gymcloud_api/v2/entities/comment.rb
@@ -2,14 +2,12 @@ module GymcloudAPI::V2
 module Entities
 
 class Comment < Grape::Entity
-
   expose :id
   expose :title
   expose :comment
   expose :commentable_id
   expose :commentable_type
   expose :user, using: Entities::User
-
 end
 
 end

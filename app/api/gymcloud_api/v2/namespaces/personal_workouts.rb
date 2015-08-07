2fdc45d4d6aff9d3b01476baf68e85d5323983eb
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-07 21:22:56 +0300

    [add] PersonalWorkout api implementation

--- app/api/gymcloud_api/v2/namespaces/personal_workouts.rb
+++ app/api/gymcloud_api/v2/namespaces/personal_workouts.rb
@@ -6,16 +6,36 @@ class PersonalWorkouts < Base
   desc 'Create Personal Workout'
   post
 
+  params do
+    requires :id, type: Integer, desc: 'Personal Workout ID'
+  end
   route_param :id do
 
     desc 'Read Personal Workout'
-    get
+    get do
+      present ::PersonalWorkout.find(params[:id]), with: Entities::PersonalWorkout
+    end
 
     desc 'Update Personal Workout'
-    patch
+    params do
+      optional :name, type: String
+      optional :description, type: String
+      optional :note, type: String
+      optional :status, type: String, desc: 'Activity status',
+                values: ::PersonalWorkout.statuses.keys
+      optional :video_url, type: String
+    end
+    patch do
+      workout = ::PersonalWorkout.find params[:id]
+      workout.update_attributes! filtered_params
+
+      present workout, with: Entities::PersonalWorkout
+    end
 
     desc 'Delete Personal Workout'
-    delete
+    delete do
+      present ::PersonalWorkout.destroy(params[:id]), with: Entities::PersonalWorkout
+    end
 
   end
 

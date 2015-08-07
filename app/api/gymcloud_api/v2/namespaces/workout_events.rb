be11fd1474be169b3bc0ce90eee41ad6fdbdd830
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-07 21:55:54 +0300

    [add] WorkoutEvent api implementation

--- app/api/gymcloud_api/v2/namespaces/workout_events.rb
+++ app/api/gymcloud_api/v2/namespaces/workout_events.rb
@@ -6,16 +6,32 @@ class WorkoutEvents < Base
   desc 'Create Workout Event'
   post
 
+  params do
+    requires :id, type: Integer, desc: 'Workout Event ID'
+  end
   route_param :id do
 
     desc 'Read Workout Event'
-    get
+    get do
+      present ::WorkoutEvent.find(params[:id]), with: Entities::WorkoutEvent
+    end
 
     desc 'Update Workout Event'
-    patch
+    params do
+      optional :begins_at, type: DateTime
+      optional :ends_at, type: DateTime
+      optional :is_completed, type: Boolean, default: 'false'
+    end
+    patch do
+      event = ::WorkoutEvent.find params[:id]
+      event.update_attributes! filtered_params.to_h
+      present event, with: Entities::WorkoutEvent
+    end
 
     desc 'Delete Workout Event'
-    delete
+    delete do
+      present ::WorkoutEvent.destroy(params[:id]), with: Entities::WorkoutEvent
+    end
 
     desc 'Fetch full Event content'
     get :full do

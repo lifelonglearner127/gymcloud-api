f56791a300a1c19984a9ae9d66ddbd67f5d6d24a
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-13 10:09:01 +0300

    [add] Services::UserBootstrap::UserProfile

---
+++ app/services/services/user_bootstrap/user_profile.rb
@@ -0,0 +1,27 @@
+module Services
+module UserBootstrap
+
+class UserProfile < BaseService
+
+  input_params :user, :profile_attrs
+  defaults profile_attrs: {}
+  run :bootstrap
+
+  private
+
+  def bootstrap
+    @user.user_profile.nil? ? create : update
+  end
+
+  def create
+    @user.create_user_profile!(@profile_attrs)
+  end
+
+  def update
+    @user.user_profile.update_attributes!(@profile_attrs)
+  end
+
+end
+
+end
+end

808b9372b2992d9e17900c9b81cffa415bb2db35
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-18 16:16:55 +0300

    [fix] rake db:seed

--- app/services/services/seed/base_seed_service.rb
+++ app/services/services/seed/base_seed_service.rb
@@ -3,20 +3,29 @@ module Seed
 
 class BaseSeedService < BaseService
 
-  KLASS = nil
-  FIXTURES = []
-
   run :create_records_from_fixtures
 
   private
 
   def create_records_from_fixtures
-    FIXTURES.map do |fixture|
+    fixtures.map do |fixture|
       attrs = fixture.dup
-      KLASS \
+      klass \
         .create_with(symbol: attrs.delete(:symbol))
         .find_or_create_by(attrs)
     end
+  end
+
+  def fixtures
+    @fixtures ||= YAML.load_file(file_path)
+  end
+
+  def klass
+    nil
+  end
+
+  def file_path
+    nil
   end
 
 end

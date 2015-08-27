84f2941685c2481bdda8b9445a242bae2517ae65
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-27 16:03:27 +0300

    [fix] admin: personal programs and workouts create

--- app/admin/personal_workout.rb
+++ app/admin/personal_workout.rb
@@ -6,5 +6,20 @@ ActiveAdmin.register PersonalWorkout do
     :workout_template_id, :person_id,
     :status, :video_url
 
-end
+  controller do
 
+    def create
+      attributes = params[:personal_workout]
+      template = ::WorkoutTemplate.find(attributes[:workout_template_id])
+      user = ::User.find(attributes[:person_id])
+      service = Services::PersonalAssignment::Workout.new(
+        template: template,
+        user: user
+      )
+      @personal_workout = service.process.result
+      super
+    end
+
+  end
+
+end

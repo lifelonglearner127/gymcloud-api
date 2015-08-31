be847a16c25cefd91c74443f3bc846f10c7716db
lifelonglearner127 <life.long.learner127@outlook.com>
2015-08-31 20:51:35 +0300

    Expose exercise property name

--- app/api/gymcloud_api/v2/entities/personal_property.rb
+++ app/api/gymcloud_api/v2/entities/personal_property.rb
@@ -5,6 +5,7 @@ class PersonalProperty < Grape::Entity
 
   expose :id
   expose :person_id
+  expose :display_name, as: :name
   expose :position
   expose :is_visible
   expose :global_property, using: Entities::GlobalProperty

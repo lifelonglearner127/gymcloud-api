fc49e45b5d912a015cec42ce1be77683cc033345
lifelonglearner127 <life.long.learner127@outlook.com>
2015-09-08 13:54:08 +0300

    Refactoring to full workout event

--- app/api/gymcloud_api/v2/entities/workout_event_exercise.rb
+++ app/api/gymcloud_api/v2/entities/workout_event_exercise.rb
@@ -6,9 +6,14 @@ class WorkoutEventExercise < Grape::Entity
   expose :id
   expose :workout_event_id
   expose :workout_exercise_id
-  expose :workout_exercise, using: Entities::WorkoutExercise
+  expose :exercise_results, using: Entities::ExerciseResult
+  expose :exercise_properties, using: Entities::ExerciseProperty
   expose :comments, using: Entities::Comment
   expose :previous, using: Entities::WorkoutEventExercise, if: :expose_previous
+  expose :previous, if: :expose_previous do |instance, options|
+    Entities::WorkoutEventExercise.represent instance.previous,
+      options.merge(expose_previous: false)
+  end
 
 end
 
--- app/api/gymcloud_api/v2/entities/workout_event_full.rb
+++ app/api/gymcloud_api/v2/entities/workout_event_full.rb
@@ -3,7 +3,6 @@ module Entities
 
 class WorkoutEventFull < Entities::WorkoutEvent
 
-  expose :exercise_results, using: Entities::ExerciseResult
   expose :workout_event_exercises, as: :exercises,
           using: Entities::WorkoutEventExercise
 

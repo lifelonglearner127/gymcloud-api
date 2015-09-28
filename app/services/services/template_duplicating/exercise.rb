bdcafed5c6527d50d3b9d988d2203c9d7b91e878
lifelonglearner127 <life.long.learner127@outlook.com>
2015-09-28 17:57:38 +0300

    [add] Exercise duplicating service

---
+++ app/services/services/template_duplicating/exercise.rb
@@ -0,0 +1,44 @@
+module Services
+module TemplateDuplicating
+
+class Exercise < BaseService
+
+  def run
+    duplicate
+  end
+
+  def input_params
+    [:exercise, :user, :folder_id]
+  end
+
+  def defaults
+    {folder_id: nil}
+  end
+
+  def build_personal
+    ::Exercise.new(prepare_attributes)
+  end
+
+  private
+
+  def duplicate
+    ::Exercise.create!(prepare_attributes)
+  end
+
+  def prepare_attributes
+    to_include = %w(name description video_url)
+    @exercise.attributes.slice(*to_include).merge(
+      is_public: false,
+      folder_id: prepare_folder,
+      author: @user
+    )
+  end
+
+  def prepare_folder
+    @folder_id ||= ::Folder.find_by(name: 'Exercises', user: @user).id
+  end
+
+end
+
+end
+end

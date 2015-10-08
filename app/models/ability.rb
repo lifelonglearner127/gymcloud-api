801079ca1c272bd9f0fb8702f073d79144039f81
lifelonglearner127 <life.long.learner127@outlook.com>
2015-10-08 14:45:59 +0300

    Unrevert "[fix] As a Pro I can change my clients profiles"
    This reverts commit 87f6bf4205947e23d0ceeadde7df80421bf171ea.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -78,6 +78,7 @@ class Ability
   end
 
   def as_pro
+    as_managing_pro_can :update, UserProfile, :user_id
     can :crud, ClientGroup, pro_id: @user.id
     can :crud, ClientGroupMembership, client_group: {pro_id: @user.id}
     can :crud, ProgramWorkout,

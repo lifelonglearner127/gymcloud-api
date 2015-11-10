73f464d63d9310b810a7c04232c4f6197971483b
lifelonglearner127 <life.long.learner127@outlook.com>
2015-11-10 22:39:03 +0200

    [kJyzqwYA][style] Relations between program_workout and workout_exercise

--- app/services/services/program_workout/create.rb
+++ app/services/services/program_workout/create.rb
@@ -23,20 +23,24 @@ class Create < BaseService
 
   def prepare_attributes
     program_template = ::ProgramTemplate.find(@attrs['program_template_id'])
-    workout_template = ::WorkoutTemplate.find(@attrs['workout_template_id'])
-    new_workout_template = Services::TemplateDuplicating::Workout.!(
-      workout: workout_template,
-      user: workout_template.user,
-      is_visible: false
-    )
+    workout_template = create_workout_template
     to_include = %w(name description note video_url)
     workout_template.attributes.slice(*to_include)
       .merge(
         program: program_template,
-        workout: new_workout_template,
+        workout: workout_template,
         position: @attrs['position'],
         week_id: @attrs['week_id']
       )
+  end
+
+  def create_workout_template
+    workout_template = ::WorkoutTemplate.find(@attrs['workout_template_id'])
+    Services::TemplateDuplicating::Workout.!(
+      workout: workout_template,
+      user: workout_template.user,
+      is_visible: false
+    )
   end
 
 end

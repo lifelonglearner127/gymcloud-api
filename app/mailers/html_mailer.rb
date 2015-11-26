4e4a418395937842806b8d90d76c44008e4c964c
lifelonglearner127 <life.long.learner127@outlook.com>
2015-11-26 23:03:52 +0200

    [refactor][vbsvw7zb] html emails prettify

--- app/mailers/html_mailer.rb
+++ app/mailers/html_mailer.rb
@@ -1,13 +1,12 @@
 class HtmlMailer < ApplicationMailer
-  default from: ENV['MAILER_EMAIL']
+
+  def welcome_new_user(user_id)
+    @user = ::User.find(user_id)
+    mail(to: @user.email, subject: "We're happy you're here!")
+  end
 
   def program_assigned(email)
-    inline_images(
-      'gymcloud-flat-logo.png',
-      'program-assigned-icon.png',
-      'blue-bg-with-icons.jpg'
-    )
-
     mail(to: email, subject: 'GymCloud')
   end
+
 end

772563c55e57ed9cdef745f8e6d9ea5633a474c2
lifelonglearner127 <life.long.learner127@outlook.com>
2015-11-26 14:42:38 +0200

    [fix][vbsvw7zb] Dynamically attach images

--- app/mailers/concerns/inline_images.rb
+++ app/mailers/concerns/inline_images.rb
@@ -3,7 +3,7 @@ module InlineImages
 
   private
 
-  def inline_images(*images)
+  def inline_images(images)
     images.each do |i|
       attachments.inline[i] = image_file(i)
     end
@@ -12,4 +12,11 @@ module InlineImages
   def image_file(file_name)
     File.read("#{Rails.root}/app/assets/images/#{file_name}")
   end
+
+  def attach_images(file_name)
+    regex = /attachments\[\'(.*)\'\]/
+    images = File.read(file_name).scan(regex).flatten
+    inline_images(images)
+  end
+
 end
--- app/mailers/devise_html_mailer.rb
+++ app/mailers/devise_html_mailer.rb
@@ -9,11 +9,18 @@ class DeviseHtmlMailer < Devise::Mailer
 
   def invitation_instructions(record, token, opts = {})
     @token = token
-    inline_images(
-      'logo-white.png',
-      'get-started-arrow.png',
-      'blue-bg-with-icons.jpg'
-    )
     devise_mail(record, :invitation_instructions, opts)
   end
+
+  def each_template(paths, name, &block) #:nodoc:
+    templates = lookup_context.find_all(name, paths)
+    if templates.empty?
+      fail ActionView::MissingTemplate.new(paths, name, paths, false, 'mailer')
+    else
+      templates = templates.uniq(&:formats)
+      attach_images(templates.first.identifier)
+      templates.each(&block)
+    end
+  end
+
 end

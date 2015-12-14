72ab95091c41cbb30216586a9f52c2db37840984
lifelonglearner127 <life.long.learner127@outlook.com>
2015-12-14 03:09:07 +0300

    [feature] ability to delete EWP folder

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -15,9 +15,9 @@ class Folder < ActiveRecord::Base
   has_closure_tree
 
   belongs_to :user
-  has_many :exercises
-  has_many :workout_templates, -> { is_visible }
-  has_many :program_templates
+  has_many :exercises, dependent: :destroy
+  has_many :workout_templates, -> { is_visible }, dependent: :destroy
+  has_many :program_templates, dependent: :destroy
 
   validates :name, :user_id, presence: true
   validates :name, length: {maximum: 255}
@@ -31,7 +31,18 @@ class Folder < ActiveRecord::Base
       []
   end
 
+  def destroy
+    ActiveRecord::Base.transaction do
+      destroy_descendant_folders
+      super
+    end
+  end
+
   private
+
+  def destroy_descendant_folders
+    descendants.reverse_each(&:destroy)
+  end
 
   def only_one_root_per_user
     condition = name == 'Root' &&

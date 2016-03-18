dd9292ee487d127a3ca5614084a921d4f9a74dce
lifelonglearner127 <life.long.learner127@outlook.com>
2016-03-18 13:39:32 +0200

    [LoiXqTmQ][fix] migration

--- db/migrate/20160318110755_add_uniq_index_to_program_workouts.rb
+++ db/migrate/20160318110755_add_uniq_index_to_program_workouts.rb
@@ -1,5 +1,6 @@
 class AddUniqIndexToProgramWorkouts < ActiveRecord::Migration
   def change
+    remove_index :program_workouts, [:workout_type, :workout_id]
     add_index :program_workouts, [:workout_type, :workout_id], unique: true
   end
 end

63ac10ff0804a471c27a4ba04a263bb6989495d6
lifelonglearner127 <life.long.learner127@outlook.com>
2016-03-23 15:16:05 +0200

    [26hIasRG][add] PropertyUnit to admin

--- app/admin/global_property.rb
+++ app/admin/global_property.rb
@@ -2,7 +2,43 @@ ActiveAdmin.register GlobalProperty do
 
   menu parent: 'Global'
 
-  permit_params :name, :unit, :symbol, :position
+  permit_params :name, :unit, :symbol, :position, property_unit_ids: []
+
+  index do
+    selectable_column
+    id_column
+    column :symbol
+    column :name
+    column :unit
+    column :available_units do |model|
+      model.property_units.pluck(:short_name).join('/')
+    end
+    column :position
+    column :created_at
+    actions
+  end
+
+  filter :property_units
+  filter :symbol
+  filter :name
+  filter :unit
+  filter :position
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs "#{f.object.class.name.titleize} Details" do
+      f.input :symbol
+      f.input :name
+      f.input :unit
+      f.input :property_units,
+        as: :select,
+        collection: ::PropertyUnit.all,
+        multiple: true
+      f.input :position
+    end
+    f.actions
+  end
 
   controller do
 

344b352012f6df95137baf27c88c5c42fd7e5291
lifelonglearner127 <life.long.learner127@outlook.com>
2016-04-03 21:32:23 +0300

    [Mqa3v9ss][typo]

--- db/migrate/20160403143602_update_exercise_properties.rb
+++ db/migrate/20160403143602_update_exercise_properties.rb
@@ -6,31 +6,32 @@ class UpdateExerciseProperties < ActiveRecord::Migration
     Services::Seed::PropertyUnit.!
     Services::Seed::GlobalPropertyUnit.!
 
-    ExerciseProperty.find_in_batches(batch_size: 1000) do |properties|
-      properties.each do |ex_prop|
-        default_unit = ex_prop.personal_property.default_unit
-        save_unit = ex_prop.personal_property.save_unit
+    begin
+      ExerciseProperty.find_in_batches(batch_size: 1000) do |properties|
+        properties.each do |ex_prop|
+          default_unit = ex_prop.personal_property.default_unit
+          save_unit = ex_prop.personal_property.save_unit
 
-        ex_prop.property_unit = default_unit
-
-        if save_unit.short_name == 'g' && default_unit.short_name == 'lbs'
-          coefficient = 453.592
-        elsif save_unit.short_name ==  'mm' && default_unit.short_name == 'in'
-          coefficient = 25.4
-        elsif save_unit.short_name ==  'mm' && default_unit.short_name == 'ft'
-          coefficient = 304.8
-        else
-          coefficient = 1
-        end
-
-        ex_prop.value = coefficient * ex_prop.value if ex_prop.value
-        ex_prop.value2 = coefficient * ex_prop.value2 if ex_prop.value2
-        begin
+          ex_prop.property_unit = default_unit
           ex_prop.save
-        rescue ActiveRecord::StatementInvalid
-          # Too big number from Ross
+
+          if save_unit.short_name == 'g' && default_unit.short_name == 'lbs'
+            coefficient = 453.592
+          elsif save_unit.short_name ==  'mm' && default_unit.short_name == 'in'
+            coefficient = 25.4
+          elsif save_unit.short_name ==  'mm' && default_unit.short_name == 'ft'
+            coefficient = 304.8
+          else
+            coefficient = 1
+          end
+
+          ex_prop.value = coefficient * ex_prop.value if ex_prop.value
+          ex_prop.value2 = coefficient * ex_prop.value2 if ex_prop.value2
+          ex_prop.save
         end
       end
+    rescue ActiveRecord::StatementInvalid
+      # Too big number from Ross
     end
   end
 end

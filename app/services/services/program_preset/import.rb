20ffe7da7780d0187c5aca2a42cb8bf2204b1d45
lifelonglearner127 <life.long.learner127@outlook.com>
2016-04-15 17:34:30 +0300

    [j7RFLBOn][add] Preset load workouts and exercises

--- app/services/services/program_preset/import.rb
+++ app/services/services/program_preset/import.rb
@@ -22,10 +22,34 @@ class Import < BaseService
   end
 
   def duplicate(program_template)
-    Services::TemplateDuplicating::Program.!(
+    program = Services::TemplateDuplicating::Program.!(
       program: program_template,
       user: @user,
       folder_id: @user.programs_folder.id
+    )
+    program.program_workouts.each do |pw|
+      duplicate_workout(pw.workout)
+    end
+    program
+  end
+
+  def duplicate_workout(workout_template)
+    workout = Services::TemplateDuplicating::Workout.!(
+      workout: workout_template,
+      user: @user,
+      folder_id: @user.workouts_folder.id
+    )
+    workout.workout_exercises.each do |we|
+      duplicate_exercise(we.exercise)
+    end
+    workout
+  end
+
+  def duplicate_exercise(exercise)
+    Services::TemplateDuplicating::Exercise.!(
+      exercise: exercise,
+      user: @user,
+      folder_id: @user.exercises_folder.id
     )
   end
 

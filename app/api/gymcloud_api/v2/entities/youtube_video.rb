a9ac24682a393f461bafeaee05094f44782b92de
lifelonglearner127 <life.long.learner127@outlook.com>
2016-05-27 18:03:33 +0300

    [D8gKzsNA][fix] youtube https links

--- app/api/gymcloud_api/v2/entities/youtube_video.rb
+++ app/api/gymcloud_api/v2/entities/youtube_video.rb
@@ -7,10 +7,10 @@ class YoutubeVideo < Grape::Entity
   expose :title, as: :name
   expose :description
   expose :embed_url do |obj|
-    "http://www.youtube.com/embed/#{obj.id}"
+    "https://www.youtube.com/embed/#{obj.id}"
   end
   expose :full_url do |obj|
-    "http://youtu.be/#{obj.id}"
+    "https://youtu.be/#{obj.id}"
   end
   expose :author_name do |_obj|
     'Youtube user'

f7324d16305be1c647a78569e9482db821c2c11a
lifelonglearner127 <life.long.learner127@outlook.com>
2016-06-24 19:19:20 +0300

    [sGKjxGbB] Expose person_avatar for WorkoutEvent by default

--- app/api/gymcloud_api/v2/entities/workout_event.rb
+++ app/api/gymcloud_api/v2/entities/workout_event.rb
@@ -16,9 +16,7 @@ class WorkoutEvent < Grape::Entity
   expose :person_name do |event|
     event.personal_workout.person.user_profile.full_name
   end
-  expose :person_avatar,
-    if: {dashboard: true} \
-  do |event|
+  expose :person_avatar do |event|
     event.personal_workout.person.user_profile.avatar.thumb.url
   end
   expose :pro_name,

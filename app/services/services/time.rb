16795f9fd7824ac73d230697b8608d04582a9202
lifelonglearner127 <life.long.learner127@outlook.com>
2016-07-15 14:27:11 +0300

    [JC7KKF3t][add] Services::Time

---
+++ app/services/services/time.rb
@@ -0,0 +1,34 @@
+module Services
+
+class Time < BaseService
+
+  DEFAULT_TIMEZONE = 'UTC'
+
+  def run
+    local_time
+  end
+
+  def input_params
+    [:user, :time]
+  end
+
+  private
+
+  def timezone
+    @timezone ||=
+      begin
+        user_timezone_name = @user.user_profile.timezone
+        zone = Timezone[user_timezone_name]
+        zone.valid? && zone.name || DEFAULT_TIMEZONE
+      end
+  end
+
+  def local_time
+    if @time.respond_to?(:zone)
+      @time.in_time_zone(timezone)
+    end
+  end
+
+end
+
+end

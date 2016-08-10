e02830c8bb04efba893fcb67e46275fe5e0e0fa5
lifelonglearner127 <life.long.learner127@outlook.com>
2016-08-10 14:46:20 +0300

    [Odxp7Zdg][feature] VideosSearch: accepts full video url for YouTube

--- app/services/services/videos_search/youtube.rb
+++ app/services/services/videos_search/youtube.rb
@@ -5,7 +5,12 @@ class Youtube < BaseVideoService
 
   def search
     yt_search = Yt::Collections::Videos.new
-    yt_search = yt_search.where(q: @q, format: 5) # only embeddable
+    id_from_url = @q.match(%r{youtube.com/watch.*v=(\w*)})[1]
+    if id_from_url.present?
+      yt_search = yt_search.where(id: id_from_url)
+    else
+      yt_search = yt_search.where(q: @q, format: 5) # only embeddable
+    end
     yt_search.first(@per_page * @page).last(@per_page)
   end
 

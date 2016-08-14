f2521d0b6ba970d8115b126351bbe0255f9c4856
lifelonglearner127 <life.long.learner127@outlook.com>
2016-08-14 12:59:22 +0300

    [YjIgd2P5][fix] Pro's dashboard

--- app/api/gymcloud_api/v2/entities/dashboard.rb
+++ app/api/gymcloud_api/v2/entities/dashboard.rb
@@ -4,8 +4,9 @@ module Entities
 class Dashboard < Grape::Entity
 
   expose :events_scheduled_this_week_count
-  expose :events_scheduled_last_week_count, if: {is_pro: true}
   expose :events_completed_this_week_count
+  expose :events_scheduled_last_week_count, if: {is_pro: true}
+  expose :events_scheduled_tomorrow_count, if: {is_pro: true}
   expose :events_scheduled_today, using: Entities::WorkoutEvent
 
 end
--- app/services/services/dashboards/pro.rb
+++ app/services/services/dashboards/pro.rb
@@ -5,6 +5,10 @@ class Pro < Client
 
   private
 
+  def fetch_dashboard
+    super.merge(events_scheduled_tomorrow_count: events.tomorrow.count)
+  end
+
   def events
     ::WorkoutEvent.with_clients(@user)
   end

0ba8fd62d6cabb3295fcff3631c5f260e05cd40f
lifelonglearner127 <life.long.learner127@outlook.com>
2016-08-25 23:18:51 +0300

    [bHhtJgNA] Mobile tutorial

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -2,15 +2,16 @@
 #
 # Table name: user_settings
 #
-#  id                   :integer          not null, primary key
-#  user_id              :integer
-#  user_account_type_id :integer
-#  units_system         :integer
-#  is_tutorial_finished :boolean
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  is_presets_loaded    :boolean
-#  tutorial_steps       :text             default([]), is an Array
+#  id                          :integer          not null, primary key
+#  user_id                     :integer
+#  user_account_type_id        :integer
+#  units_system                :integer
+#  is_tutorial_finished        :boolean
+#  is_presets_loaded           :boolean
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  tutorial_steps              :text             default([]), is an Array
+#  is_mobile_tutorial_finished :boolean
 #
 
 class UserSettings < ActiveRecord::Base
--- spec/models/regression/user_settings_spec.rb
+++ spec/models/regression/user_settings_spec.rb
@@ -17,10 +17,11 @@ RSpec.describe UserSettings do
 	it { is_expected.to have_db_column :user_account_type_id }
 	it { is_expected.to have_db_column :units_system }
 	it { is_expected.to have_db_column :is_tutorial_finished }
+	it { is_expected.to have_db_column :is_presets_loaded }
 	it { is_expected.to have_db_column :created_at }
 	it { is_expected.to have_db_column :updated_at }
-	it { is_expected.to have_db_column :is_presets_loaded }
 	it { is_expected.to have_db_column :tutorial_steps }
+	it { is_expected.to have_db_column :is_mobile_tutorial_finished }
 
   # === Database (Indexes) ===
   it { is_expected.to have_db_index ["user_account_type_id"]}

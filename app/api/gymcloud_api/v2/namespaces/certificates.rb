7d52ed04e12dacea9c166d1e8015fb5dfbdd957a
lifelonglearner127 <life.long.learner127@outlook.com>
2016-09-11 18:10:02 +0300

    [2m2bcNNB] Changes to cert upload endpoint

--- app/api/gymcloud_api/v2/namespaces/certificates.rb
+++ app/api/gymcloud_api/v2/namespaces/certificates.rb
@@ -7,12 +7,12 @@ namespace :certificates do
 
   desc 'Upload Certificate'
   params do
-    requires :files, type: [Rack::Multipart::UploadedFile]
+    requires :file, type: Rack::Multipart::UploadedFile
   end
   post do
     certificate = current_user.certificates.build
     authorize!(:upload, certificate)
-    certificate.file = ActionDispatch::Http::UploadedFile.new(params[:files][0])
+    certificate.file = ActionDispatch::Http::UploadedFile.new(params[:file])
     certificate.status = :unverified
     certificate.save!
     present(certificate)

52c3be7f362445f95d9f3909da3ebdab32aa6189
lifelonglearner127 <life.long.learner127@outlook.com>
2016-09-24 15:59:56 +0300

    [bZIC5ko0] CarrierWave AWS expires in 10 years

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,11 @@ CarrierWave.configure do |config|
   config.aws_acl    = 'public-read'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
+  config.aws_attributes = {
+    cache_control: 'max-age=315576000',
+    expires: 10.years.from_now.httpdate
+  }
+
   config.aws_credentials = {
     access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
